00784982b46dc93eebec0feb6c7e0f4c2c46b73f
Bill Leatham <weleatham@gmail.com>
2023-10-27 08:49:17 -0400

    Latest update. Made changes to the multstn script with the new foldercreate. This allows for multiple directory structure.

--- foldercreate.py
+++ foldercreate.py
@@ -4,24 +4,26 @@
 ###############################################################################
 # Script Created by William Leatham IV in October of 2023
 # Description:
-# This program created to check if a directory exists and creates it if it 
-# does not.
+# This program created to check if a directory structure exists and creates it 
+# if it does not.
 ###############################################################################
 """
 import os
-def folder_check():
-    usrdir = os.path.expanduser('~')+"/DataRequests/"
-# Checking if the main output directory exists and if it does not then it 
-# creates it. Along with subdirectories.
-    if not os.path.exists(usrdir):
-        os.makedirs(usrdir)
-        print("Output directory created: "+usrdir)
-        
-    if not os.path.exists(usrdir+"/ACIS/"):
-        os.makedirs(usrdir+"ACIS/")
-        print("Output subdirectory created!: "+usrdir+"ACIS/")
-        
-    return(usrdir+"ACIS/")
+def folder_check(*args):
+    usrdir = os.path.expanduser('~')+"/"
+    cwd = os.getcwd()+"/"
+    if len(args) == 0:
+        print("Saving in current working dir, make sure this is right: "+cwd)
+        return(cwd)
+    elif len(args) >= 1:
+        wrkdir = "/".join(str(arg) for arg in args)
+        if not os.path.exists(usrdir+wrkdir):
+            os.makedirs(usrdir+wrkdir)
+            print("Output directory created: "+usrdir+wrkdir+"/")
+            return(usrdir+wrkdir+"/")
+        else:
+            return(usrdir+wrkdir+"/")
+            print("Directory exists. Saving in: "+usrdir+wrkdir+"/")
 if __name__ == '__main__':
     # Running the folder check.
     folder_check()
