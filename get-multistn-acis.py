63cf3502efd17eca9bf0e03b9bcbcd3509c4bb48
Bill Leatham <Weleatham@gmail.com>
2023-11-06 16:57:23 -0500

    Changed how program brings together multi-day data. Now it is all summed up via this program and saved by the start/end dates the user selects. Changed to CWA based to a lat/lon bounding box and allow the user to select the rounding precision they would like.

--- get-multistn-acis.py
+++ get-multistn-acis.py
@@ -11,50 +11,78 @@
 ###############################################################################
 # Edits:
 # Added a folder check for where the data is downloaded to. 
+# Nov 6, 2023
+# Changed to area bounds for getting data vs CWA. Put the daily data into a
+# dataframe that is summed up and returned to the user.
 ###############################################################################
 """
 import requests
 from datetime import datetime
-from datetime import timedelta
 import pandas as pd
 import numpy as np
 from io import StringIO
 import foldercreate
-# Station identifiers via cwa or state and date of interest.
-station_ids = ('BOX','ALY','OKX','GYX','BTV')
-sdate = datetime(1894,11,5)
-num_days = 2
+import json
+# Station identifiers via bounding box.
+bounds = "-74.5,40.3,-68.0,43.2"
+var = "snow" #pcpn
+sumhead = "SNOWFALL"
+sdate = datetime(2015,2,14)
+edate = datetime(2015,2,16)
+precision = 1
 # First checking if the folder directories are created or not.
-outdir = foldercreate.folder_check("DataRequests","ACIS","1894-ElectionDay")
-def fetch_weather_data(station_id, the_date):
+outdir = foldercreate.folder_check("DataRequests","ACIS","Feb-14-16-2015")
+def fetch_weather_data(bound_box,start_date,end_date,variable):
     base_url = 'https://data.rcc-acis.org/MultiStnData?'
     params = {
         #'state': station_id,
-        'cwa': station_id,
-        'date': the_date.strftime("%Y-%m-%d"),
-        'elems': 'snow',#'pcpn',
-        'output':'csv'
-        #pcpn
+        'bbox': bound_box,
+        'sdate': start_date.strftime("%Y-%m-%d"),
+        'edate': end_date.strftime("%Y-%m-%d"),
+        'meta':'uid,name,ll,state',
+        'elems': [{"name":variable,
+                   "interval":[0,0,1]}],
+        'output':'json'
     }
     response = requests.post(base_url,json=params)
     response.raise_for_status()  # Raise an error for bad HTTP status codes
-    return response.content
+    return response.text
 def main():
-    the_date = [sdate + timedelta(days=idx) for idx in range(num_days)]
-    for sid in station_ids:
-        for i in the_date:
-            weather_data = fetch_weather_data(sid, i)
-            # Parse the CSV data into a dataframe
-            col_names = ['UID','CITYNAME','STATE','LON','LAT','Elev_ft','Val_in']
-            df = pd.read_csv(StringIO(weather_data.decode('utf-8')),names=col_names,header=None)
-            df['Date'] = i
-            df['Val_in'] = df['Val_in'].replace('M',np.nan)
-            df['Val_in'] = df['Val_in'].replace('T',0.001)
-            df[['Val_in','LON','LAT']] = df[['Val_in','LON','LAT']].apply(pd.to_numeric)
-            df['CWA'] = sid
-            # Save data to a CSV file
-            output_file = f'{sid}-{i.year}-{i.month}-{i.day}.csv'
-            df.to_csv(outdir+output_file)
-            print(f'Data saved to {outdir}{output_file}')
+    the_date_lst = [i.strftime("%Y-%m-%d") for i in pd.date_range(start=sdate,end=edate,freq='D')]
+    json_parsed = []
+    weather_data = json.loads(fetch_weather_data(bounds,sdate,edate,var))
+    for item in weather_data.values():
+        for x in item:
+            json_parsed.append({
+            'LON' : x['meta']['ll'][0],
+            'LAT' : x['meta']['ll'][1],
+            'STATE':x['meta']['state'],
+            'UID': x['meta']['uid'],
+            'NAME': x['meta']['name'],
+            'Data': x['data'],
+            })
+    df = pd.DataFrame(json_parsed)
+    # Adding the snow/precip/temp data to the dataframe a month at a time.
+    df[the_date_lst] = pd.DataFrame(df['Data'].tolist()).map(lambda x: x[0])
+    # Removing the extra data column we don't need.
+    df = df.drop(columns='Data')
+    # Converting the data into a numeric value.
+    df[the_date_lst] = df[the_date_lst].replace('M',np.nan)
+    df[the_date_lst] = df[the_date_lst].replace('T',0.0001)
+    df[the_date_lst] = df[the_date_lst].apply(pd.to_numeric)
+    # Summing up the columns of data. If one month is missing data then
+    # the SUM is equal to NaN.
+    df[sumhead] = df[the_date_lst].sum(axis=1, min_count=1)
+    df[sumhead] = np.where((df[sumhead] > 0.0004),
+                                    df[sumhead].round(precision),
+                                    df[sumhead])
+    df[sumhead] = np.where((df[sumhead] <= 0.0004),
+                                    'T',
+                                    df[sumhead])
+    df.set_index('UID',inplace=True) 
+    # Save data to a CSV file
+    output_file = f'{sumhead}-{sdate.year}-{sdate.month}-{sdate.day}-to-{edate.year}-{edate.month}-{edate.day}.csv'
+    df.to_csv(outdir+output_file)
+    print(f'Data saved to {outdir}{output_file}')
 if __name__ == "__main__":
    main()
